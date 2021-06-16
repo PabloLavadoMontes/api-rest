4a502c1b7e5f97693bd2d5406a7edc2cd67fd263
ferangel <fer.quaglietta@gmail.com>
2021-06-16 17:00:26 +0200

    AxiosClient example

---
+++ client/axios/clientAxios.js
@@ -0,0 +1,16 @@
+function getUsuarios() {
+    
+    axios.get('http://localhost:2800/usuarios')
+    .then(function (response) {
+        // handle success
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
+}
+
